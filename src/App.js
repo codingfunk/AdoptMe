0c017162a7d2df688986fb5f02d9f5924942f1d8
Veronika Nikonova <veronika.nikonova@acrolinx.com>
2022-04-07 15:13:32 +0200

    configured eslint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet.js";
-
+import Pet from "./Pet";
+/*
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
@@ -19,4 +19,17 @@ const App = () => {
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+*/
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Beam" animal="Dog" breed="Wheaten Terrier" />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
